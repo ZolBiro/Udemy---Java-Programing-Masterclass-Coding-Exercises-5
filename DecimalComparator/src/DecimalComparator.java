3a450581029bebc6d7c302d16953a54bcb888e4f
Zoltan Biro <36306066+ZolBiro@users.noreply.github.com>
2019-04-07 11:03:44 +0200

    Add files via upload

---
+++ DecimalComparator/src/DecimalComparator.java
@@ -0,0 +1,11 @@
+public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces (double numberOne, double numberTwo){
+        if ((int) (numberOne * 1000) == (int)(numberTwo * 1000)){
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+}
